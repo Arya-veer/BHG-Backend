99f5f19508c5cd0d49988528fd1b9746afda5cd2
Arya-veer <f20200094@pilani.bits-pilani.ac.in>
2022-12-08 15:05:31 +0530

    Changed allowed hosts

--- Library_backend/settings.py
+++ Library_backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-fhoc#9y7dwonb)-&@4aox^g$3x6zk+o)-l&no^i5wjo-npjhqo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
