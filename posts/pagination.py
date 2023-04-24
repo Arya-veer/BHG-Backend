e60a78bb21ff4cd1f36d5eb8b4d62468e1b6c387
Arya-veer <f20200094@pilani.bits-pilani.ac.in>
2023-04-24 16:11:35 +0530

    Pagination

--- posts/pagination.py
+++ posts/pagination.py
@@ -1,6 +1,13 @@
 from rest_framework.pagination import PageNumberPagination
-
+from rest_framework.response import Response
+import math
 
 class VideoPagination(PageNumberPagination):
     page_size = 2
     page_size_query_param = "page_size"
+
+    def get_paginated_response(self, data):
+        data = super().get_paginated_response(data).data
+        data['count'] = math.ceil(data['count']/self.page_size)
+        return Response(data)
+
