db5c31228e70e3b6fd54b3a4ef3ddd8dd9575632
Arya veer singh chauhan <81042581+Arya-veer@users.noreply.github.com>
2023-11-22 11:07:20 +0530

    Update settings.py

--- Library_backend/settings.py
+++ Library_backend/settings.py
@@ -124,12 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/backend-static/'
+STATIC_URL = 'backend-static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'backend-static')
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'backend-media')
-MEDIA_URL = '/backend-media/'
+MEDIA_URL = 'backend-media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
